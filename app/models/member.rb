0a0326bd535121a5d073c973069c0c7b01750dd5
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-11-26 09:02:05 +0900

    feature : let error-message show

--- app/models/member.rb
+++ app/models/member.rb
@@ -12,7 +12,7 @@ class Member < ApplicationRecord
 
   validates :name,
               presence: true,
-              format: { with: /^[A-Za-z][A-Za-z0-9]*$/, allow_blank: true },
+              format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true },
               length: { minimum: 2, maximum: 20, allow_blank: true },
               uniqueness: { case_sensitive: false }
 
