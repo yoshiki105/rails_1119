d629fbb31f62f3a90677e5694886b0cfe125f5ec
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-11-27 11:15:27 +0900

    feature : add login_require

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
   # ヘルパーメソッド
   # テンプレートの中でも使えるように登録。
   helper_method :current_member
+
+  # 自作例外を定義
+  class LoginRequired < StandardError; end
+  class Forbidden < StandardError; end
+
+  # before_action用メソッド
+  private def login_required
+    raise LoginRequired unless current_member
+  end
+
 end
