c9e49db9763714fb55e6f71f4e5b8e2374b6d747
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-11-27 12:15:26 +0900

    feature : add edit_account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -6,5 +6,16 @@ class AccountsController < ApplicationController
   end
 
   def edit
+    @member = current_member
+  end
+
+  def update
+    @member = current_member
+    @member.assign_attributes(params[:account])
+    if @member.save
+      redirect_to :account, notice: "アカウント情報を更新しました。"
+    else
+      render "edit"
+    end
   end
 end
