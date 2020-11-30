b7b05250ab5705c0cebb0897bb3446da011ad136
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-11-30 14:51:02 +0900

    view : topcontrollerを編集

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,9 +1,8 @@
 class TopController < ApplicationController
   def index
+    @articles = Article.visible.order(released_at: :desc).limit(5)
+    @articles = @articles.open_to_the_public unless current_member
   end
 
-  def about
-
-  end
-
+  def about; end
 end
