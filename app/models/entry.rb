67c1a471e9df01e1521ddb6da7434ff6c72fa117
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-01 11:50:56 +0900

    feat : model associate Member & Entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,4 @@
 class Entry < ApplicationRecord
+  # モデル間の関連付け
+  belongs_to :author, class_name: 'Member', foreign_key: 'member_id'
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,35 +2,39 @@ class Member < ApplicationRecord
   # セッションの設定
   has_secure_password
 
+  # モデル間の関連付け
+  has_many :entries, dependent: :destroy
+
   # バリデーション
   validates :number,
-                presence: true,
-                numericality: {
-                  only_integer: true,
-                  greater_than: 0,
-                  less_than: 100,
-                  allow_blank: true,
-                },
-                uniqueness: true
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than: 0,
+              less_than: 100,
+              allow_blank: true
+            },
+            uniqueness: true
 
   validates :name,
-              presence: true,
-              format: {
-                with: /\A[A-Za-z][A-Za-z0-9]*\z/,
-                allow_blank: true,
-                message: :invalid_member_name     # ja.ymlと対応
-              },
-              length: { minimum: 2, maximum: 20, allow_blank: true },
-              uniqueness: { case_sensitive: false }
+            presence: true,
+            format: {
+              with: /\A[A-Za-z][A-Za-z0-9]*\z/,
+              allow_blank: true,
+              message: :invalid_member_name # ja.ymlと対応
+            },
+            length: { minimum: 2, maximum: 20, allow_blank: true },
+            uniqueness: { case_sensitive: false }
 
   validates :full_name,
-              presence: true,
-              length: { maximum: 20 }
+            presence: true,
+            length: { maximum: 20 }
 
-  validates :email, email: { allow_blank: true }  # gemによる拡張表現
+  validates :email, email: { allow_blank: true } # gemによる拡張表現
 
   # 現在の（変更前の）パスワード属性
   attr_accessor :current_password
+
   # パスワードは常に存在するように設定
   validates :password, presence: { if: :current_password }
 
@@ -38,13 +42,13 @@ class Member < ApplicationRecord
   class << self
     def search(query)
       # memberをnumber順にソートする
-      rel = order("number")
+      rel = order('number')
       if query.present?
         # ソートしたものにnameカラムまたはfull_nameカラムを対象にレコードを絞り込む
-        rel = rel.where("name LIKE ? OR full_name LIKE ?",
-            "%#{query}%", "%#{query}%")
+        rel = rel.where('name LIKE ? OR full_name LIKE ?',
+                        "%#{query}%", "%#{query}%")
       end
-      return rel
+      rel
     end
   end
 end
